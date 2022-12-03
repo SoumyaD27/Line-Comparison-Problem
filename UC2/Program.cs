08d9bb1ad18ea617b970bbe94aeb726d2176dc08
SoumyaD27 <soumyad233@gmail.com>
2022-12-03 11:40:18 +0530

    Check whether lines are equal.

---
+++ UC2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, y1, y2, a1, a2, b1, b2;
+            Console.WriteLine("Enter the first point values: ");
+            x1 = double.Parse(Console.ReadLine());
+            x2 = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second point values: ");
+            a1 = double.Parse(Console.ReadLine());
+            a2 = double.Parse(Console.ReadLine());
+            b1 = double.Parse(Console.ReadLine());
+            b2 = double.Parse(Console.ReadLine());
+
+            double firstLength = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            double secondLength = Math.Sqrt((Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2)));
+
+            if (firstLength == secondLength)
+            {
+                Console.WriteLine("Two Lines are equal.");
+            }
+            else
+            {
+                Console.WriteLine("Two Lines are not equal.");
+            }
+        }
+    }
+}
