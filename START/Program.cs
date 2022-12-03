59c9e2143e5a13b2ddae0ad903f41f0927e0bedb
SoumyaD27 <soumyad233@gmail.com>
2022-12-03 11:32:40 +0530

    Welcome message

---
+++ START/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace START
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparsion Computation Program");
+        }
+    }
+}
